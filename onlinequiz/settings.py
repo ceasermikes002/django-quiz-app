299271c7e7c4d457186d25a09a663cc5d1e4fa6e
chima <chimaemekaiheonu@gmail.com>
2024-01-29 22:28:42 +0100

    new change

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -25,9 +25,9 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0:8000']
 
 
 # Application definition
