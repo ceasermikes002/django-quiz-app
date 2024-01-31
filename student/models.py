d2a5b513ee2e88f51a2b45b9c1e505faa8202d56
chima <chimaemekaiheonu@gmail.com>
2024-01-31 14:18:12 +0100

    new change

--- student/models.py
+++ student/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Student(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic= models.ImageField(upload_to='profile_pic/Student/',null=True,blank=True)
+    # profile_pic= models.ImageField(upload_to='profile_pic/Student/',null=True,blank=True)
     address = models.CharField(max_length=40)
     mobile = models.CharField(max_length=20,null=False)
    
--- teacher/models.py
+++ teacher/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Teacher(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic= models.ImageField(upload_to='profile_pic/Teacher/',null=True,blank=True)
+    # profile_pic= models.ImageField(upload_to='profile_pic/Teacher/',null=True,blank=True)
     address = models.CharField(max_length=40)
     mobile = models.CharField(max_length=20,null=False)
     status= models.BooleanField(default=False)
